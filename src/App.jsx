6f3d31e3d2199d6060279f5d71c76a135cde0e97
MonEcoleEnDauphine <monecoleendauphine@gmail.com>
2026-09-17 22:43:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -281,12 +281,7 @@ const VieEcole = () => (
 
 // --- MODULE : CONTACT ---
 const InfosContact = () => (
-  <div className="space-y-6 max-w-6xl mx-auto animate-fade-in pb-10">
-    <div className="bg-blue-600 p-8 rounded-xl shadow-md text-white mt-6">
-      <h1 className="text-3xl font-bold mb-2">Bienvenue sur le portail du Cours Tom Morel</h1>
-      <p className="text-blue-100">Retrouvez ici toutes les informations de scolarité.</p>
-    </div>
-
+ 
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
       <div className="bg-white p-6 rounded-2xl shadow-sm border border-slate-100 flex flex-col items-center text-center">
         <div className="w-12 h-12 rounded-full bg-blue-50 text-blue-500 flex items-center justify-center mb-4">
