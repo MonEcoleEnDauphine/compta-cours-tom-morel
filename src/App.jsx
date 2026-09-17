328142d8c87c8ebcba3d8ca5c5119f96098c3627
MonEcoleEnDauphine <monecoleendauphine@gmail.com>
2026-09-17 22:35:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5285,7 +5285,6 @@ const GestionEvenements = () => {
     </div>
   );
 };
-
 export default function App() {
   // 1. On lit l'URL pour voir si on ouvre un nouvel onglet sur un module précis
 const [activeTab, setActiveTab] = useState(() => {
